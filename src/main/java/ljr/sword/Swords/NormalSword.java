bfade66ff5ca7c741fb48a7620f739bed65e0dcb
SnowFuryHQ <3374353308@qq.com>
2020-07-31 20:30:53 +0800

    矿物词典，小修补

--- src/main/java/ljr/sword/Swords/NormalSword.java
+++ src/main/java/ljr/sword/Swords/NormalSword.java
@@ -12,11 +12,13 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.MoverType;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityDamageSource;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -29,11 +31,15 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class NormalSword extends ItemSword{
 
 	private static final String name = "normalsword";
+	private static final int HarvestLevel = 10;//挖掘等级
 
 	public NormalSword() {
-		super(EnumHelper.addToolMaterial("Normal", 10, Integer.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE, 30));
+		super(EnumHelper.addToolMaterial("Normal", HarvestLevel, Integer.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE, 30));
         this.setUnlocalizedName(name);
         this.setRegistryName(name);
+    	this.setHarvestLevel("pickaxel", HarvestLevel);
+    	this.setHarvestLevel("axe", HarvestLevel);
+    	this.setHarvestLevel("sword", HarvestLevel);
         this.setCreativeTab(CreativeTabsLoader.MySwordCreativeTabs);
 	}
 	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity)
@@ -60,7 +66,7 @@ public class NormalSword extends ItemSword{
 	        AxisAlignedBB bb =new AxisAlignedBB(playerX-10,playerY-10,playerZ-10,playerX+10,playerY+10,playerZ+10);
 	        List<Entity> Entities =worldIn.getEntitiesWithinAABBExcludingEntity(playerIn,bb);
 	        for (Entity entity : Entities) {
-	        	MySword.getLogger().info("Find"+Entities.size()+"entity(ies) to kill");
+	        	MySword.getLogger().info(String.format("Find %d entities to kill", Entities.size()));
 	        	if (entity instanceof EntityLivingBase) {
 	        		KillEntityLivingBase((EntityLivingBase) entity);
 				}else {
@@ -86,11 +92,19 @@ public class NormalSword extends ItemSword{
 			}
 	    	try {
 				if (org.lwjgl.input.Keyboard.isKeyDown(org.lwjgl.input.Keyboard.KEY_LSHIFT)) {
+					if (target instanceof EntityPlayerMP) {
+						MySword.getLogger().info("Lshift to kick player");
+						EntityPlayerMP entityPlayerMP = (EntityPlayerMP) target;
+						entityPlayerMP.onDeath(new EntityDamageSource(target.getName(),target ));
+						entityPlayerMP.connection.disconnect(null);
+					} else {
 					MySword.getLogger().info("Lshift to remove entity");
 					Remove_entity(target);
+					}
 				}
 			} catch (Exception e) {
 			}
+	    	target.setFire(20);
 	    	target.setInvisible(false);
 	    	target.setSprinting(false); 
 	    	target.handleWaterMovement();
@@ -98,6 +112,21 @@ public class NormalSword extends ItemSword{
 	    	target.stopActiveHand();
 	    	target.setAbsorptionAmount(0);
 	    	target.setJumping(true);
+	    	target.attackEntityFrom(DamageSource.OUT_OF_WORLD,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.ANVIL,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.LIGHTNING_BOLT,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.ON_FIRE,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.LAVA,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.IN_WALL,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.CRAMMING,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.DROWN,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.STARVE,Float.MAX_VALUE);
+	    	target.attackEntityFrom(DamageSource.CACTUS,Float.MAX_VALUE);
+			target.attackEntityFrom(DamageSource.FALL,Float.MAX_VALUE);
+			target.attackEntityFrom(DamageSource.WITHER,Float.MAX_VALUE);
+			target.attackEntityFrom(DamageSource.DRAGON_BREATH,Float.MAX_VALUE);
+			target.attackEntityFrom(DamageSource.FALLING_BLOCK,Float.MAX_VALUE);
+			target.attackEntityFrom(DamageSource.FIREWORKS,Float.MAX_VALUE);
 	    	target.attackEntityFrom(DamageSource.MAGIC,Float.MAX_VALUE);
 	    	target.onDeath(DamageSource.MAGIC);
 	    	target.setHealth(0f);
@@ -118,13 +147,6 @@ public class NormalSword extends ItemSword{
 				e.printStackTrace();
 			}
 	    	try {
-				if (org.lwjgl.input.Keyboard.isKeyDown(org.lwjgl.input.Keyboard.KEY_LSHIFT)) {
-					MySword.getLogger().info("Lshift to remove entity");
-					Remove_entity(target);
-				}
-			} catch (Exception e) {
-			}
-	    	try {
 				target.performHurtAnimation();
 			} catch (Exception e) {
 			}
@@ -135,15 +157,17 @@ public class NormalSword extends ItemSword{
 		}
 	    private void Remove_entity(Entity target) {
 	    	try {
+	    		final int Deep = 10000;
 		        BlockPos targPos = target.getPosition();
 		        int targPosX = targPos.getX();
-		        int targPosY = targPos.getY();
+		        int targPosY = targPos.getY()-Deep;
 		        int targPosZ = targPos.getZ();
-			target.move(MoverType.SHULKER_BOX, targPosX, targPosY-10000, targPosZ);
-			target.move(MoverType.PISTON, targPosX, targPosY-10000, targPosZ);
-			target.move(MoverType.PLAYER, targPosX, targPosY-10000, targPosZ);
-			target.move(MoverType.SELF, targPosX, targPosY-10000, targPosZ);
-	    	target.move(MoverType.SHULKER, targPosX, targPosY-10000, targPosZ);
+		    MySword.getLogger().info(String.format("Moving entity to %d %d %d",targPosX,targPosY,targPosZ));
+			target.move(MoverType.SHULKER_BOX, targPosX, targPosY, targPosZ);
+			target.move(MoverType.PISTON, targPosX, targPosY, targPosZ);
+			target.move(MoverType.PLAYER, targPosX, targPosY, targPosZ);
+			target.move(MoverType.SELF, targPosX, targPosY, targPosZ);
+	    	target.move(MoverType.SHULKER, targPosX, targPosY, targPosZ);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -183,5 +207,10 @@ public class NormalSword extends ItemSword{
 	            return 10F;
 	        }
 	    }	
+	    public float getAttackDamage()
+	    {
+	        return Float.MAX_VALUE;
+	    }
+		
 	    
 }
